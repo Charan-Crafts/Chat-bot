7566c14695b8b6df0d802ba0917144671fecaee0
Charan <mangalacharancharan@gmail.com>
2025-03-31 11:46:30 +0530

    okay

--- Folder/main.py
+++ Folder/main.py
@@ -12,6 +12,8 @@ api_key = st.secrets["OPENROUTER_API_KEY"]
 
 import streamlit as st
 
+st.write(st.secrets)  # This will print available keys
+
 
 model = ChatOpenAI(
     model="google/gemini-pro",
